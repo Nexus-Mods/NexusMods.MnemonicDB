568832bba857e39d41b37800e34142f35f03750f
halgari <tbaldridge@gmail.com>
2024-02-01 09:38:16 -0700

    Delete more code

---
+++ benchmarks/NexusMods.EventSourcing.DatomBenchmarkTest/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Diagnostics;
+using NexusMods.EventSourcing.DatomBenchmarkTest;
+using NexusMods.Paths;
+
+var sw = Stopwatch.StartNew();
+var db = new DatomStore($"c:\\tmp\\testData{Guid.NewGuid()}.rocksdb");
+var data = new GenerateData(db);
+data.Generate();
+Console.WriteLine($"Elapsed: {sw.Elapsed} Emitted: {data.EmitCount}");
