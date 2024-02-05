807e293d195eb6e881c5a57168fa8a634cd15533
halgari <tbaldridge@gmail.com>
2024-02-05 13:06:00 -0700

    WIP

--- tests/NexusMods.EventSourcing.Tests/DatomAccumulator.cs
+++ tests/NexusMods.EventSourcing.Tests/DatomAccumulator.cs
@@ -5,16 +5,23 @@ namespace NexusMods.EventSourcing.Tests;
 public struct DatomAccumulator : IResultSetSink
 {
     public readonly List<(ulong e, ulong a, object v, ulong tx)> Datoms;
+    private readonly ulong _lowEntityId;
+    private readonly ulong _highEntityId;
 
-    public DatomAccumulator()
+    public DatomAccumulator(bool limitToUserSpace = false)
     {
+        _lowEntityId = limitToUserSpace ? Ids.MinId(IdSpace.Entity) : ulong.MaxValue;
+        _highEntityId = limitToUserSpace ? Ids.MaxId(IdSpace.Entity) : ulong.MaxValue;
         Datoms = new List<(ulong e, ulong a, object v, ulong tx)>();
     }
+
 
     public void Process<T>(ref T resultSet) where T : IResultSet
     {
         do
         {
+            if (resultSet.EntityId < _lowEntityId || resultSet.EntityId > _highEntityId)
+                continue;
             Datoms.Add((resultSet.EntityId, resultSet.Attribute, resultSet.Value, resultSet.Tx));
         } while (resultSet.Next());
     }
