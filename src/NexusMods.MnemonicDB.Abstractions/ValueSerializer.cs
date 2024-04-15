f0ed976bd779233c0d065dbf79a9a11e801624af
Timothy Baldridge <tbaldridge@gmail.com>
2024-04-15 11:32:02 -0600

    WIP

--- src/NexusMods.MnemonicDB.Abstractions/ValueSerializer.cs
+++ src/NexusMods.MnemonicDB.Abstractions/ValueSerializer.cs
@@ -2,6 +2,7 @@
 using System.Buffers;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using Reloaded.Memory.Extensions;
 
@@ -14,6 +15,8 @@ public class AValueSerializer
     private static readonly Encoding AsciiEncoding = Encoding.ASCII;
     private static readonly Encoding Utf8Encoding = Encoding.UTF8;
     #endregion
+
+
 
 
     /// <summary>
@@ -32,20 +35,67 @@ public class AValueSerializer
         if (typeA != typeB)
             return typeA.CompareTo(typeB);
 
-        var minLength = Math.Min(alen, blen);
-        var result = Unsafe.AsRef<int>(a) - Unsafe.AsRef<int>(b);
-        if (result != 0)
-            return result;
+        var aVal = a + 1;
+        var bVal = b + 1;
 
-        for (var i = sizeof(int); i < minLength; i++)
+        return (ValueTags)typeA switch
         {
-            result = a[i] - b[i];
-            if (result != 0)
-                return result;
-        }
-
-        return alen - blen;
+            ValueTags.UInt8 => Compare<byte>(aVal, bVal),
+            ValueTags.UInt16 => Compare<ushort>(aVal, bVal),
+            ValueTags.UInt32 => Compare<uint>(aVal, bVal),
+            ValueTags.UInt64 => Compare<ulong>(aVal, bVal),
+            ValueTags.UInt128 => Compare<UInt128>(aVal, bVal),
+            ValueTags.Int16 => Compare<short>(aVal, bVal),
+            ValueTags.Int32 => Compare<int>(aVal, bVal),
+            ValueTags.Int64 => Compare<long>(aVal, bVal),
+            ValueTags.Int128 => Compare<Int128>(aVal, bVal),
+            ValueTags.Float32 => Compare<float>(aVal, bVal),
+            ValueTags.Float64 => Compare<double>(aVal, bVal),
+            ValueTags.Ascii => CompareBlob(aVal, bVal),
+            ValueTags.Utf8 => CompareBlob(aVal, bVal),
+            ValueTags.Utf8Insensitive => CompareUtf8Insensitive(aVal, bVal),
+            ValueTags.Blob => CompareBlob(aVal, bVal),
+            _ => alen - blen
+        };
     }
 
+    private static unsafe int CompareBlob(byte* aVal, byte* bVal)
+    {
+        var aSize = GetInlineSize(ref aVal);
+        var bSize = GetInlineSize(ref bVal);
+
+        return new Span<byte>(aVal, aSize)
+            .SequenceCompareTo(new Span<byte>(bVal, bSize));
+    }
+
+    private static unsafe int CompareUtf8Insensitive(byte* aVal, byte* bVal)
+    {
+        var aSize = GetInlineSize(ref aVal);
+        var bSize = GetInlineSize(ref bVal);
+
+        var strA = Utf8Encoding.GetString(aVal, aSize);
+        var strB = Utf8Encoding.GetString(bVal, bSize);
+
+        return string.Compare(strA, strB, StringComparison.InvariantCultureIgnoreCase);
+    }
+
+    private static unsafe int GetInlineSize(ref byte* aVal)
+    {
+        int aSize = aVal[0];
+        if (aSize == byte.MaxValue)
+        {
+            aVal += 1;
+            aSize = ((ushort*)aVal)[0];
+        }
+
+        return aSize;
+    }
+
+
+    private static unsafe int Compare<T>(byte* aVal, byte* bVal)
+    where T : unmanaged, IComparable<T>
+    {
+        return ((T*)aVal)[0].CompareTo(((T*)bVal)[0]);
+    }
 
 }
