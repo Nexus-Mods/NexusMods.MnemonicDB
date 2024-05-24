a5c213c9e9ef509d53d3b80d3c1b9f9d999dfaf4
halgari <tbaldridge@gmail.com>
2024-05-23 23:34:22 -0600

    Add Intersection to IDb, wip

--- src/NexusMods.MnemonicDB.Abstractions/IDbExtensions.cs
+++ src/NexusMods.MnemonicDB.Abstractions/IDbExtensions.cs
@@ -9,6 +9,20 @@ public static class IDbExtensions
     /// </summary>
     public static IEnumerable<EntityId> Intersection(this IDb db, params IAttribute[] attrs)
     {
+        foreach (var id in db.Find(attrs[0]))
+        {
+            var index = db.Get(id);
+            for (int i = 1; i < attrs.Length; i++)
+            {
+                for (int j = 0; j < index.Count; j++)
+                {
+                    if (index[j] = attrs[i].
+                    {
+                        yield return id;
+                    }
+                }
+            }
+        }
 
     }
 }
