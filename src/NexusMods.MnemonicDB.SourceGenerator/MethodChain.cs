91d1e48f5cc6a788c352c6d91791c8ffc7d4a29c
halgari <tbaldridge@gmail.com>
2024-05-24 07:29:06 -0600

    Delete accidentally added code

--- src/NexusMods.MnemonicDB.SourceGenerator/MethodChain.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NexusMods.MnemonicDB.SourceGenerator;
-
-public record MethodChain()
-{
-    public string Namespace { get; set; } = "";
-    public MethodCall[] Methods { get; set; } = [];
-}
