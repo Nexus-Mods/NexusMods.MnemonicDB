e36a4d5ef820b26c8608b1e9128d38f8d1b68196
halgari <tbaldridge@gmail.com>
2024-06-13 11:46:50 -0600

    Add `As`, `Is` and other getters/setters. Fixed the Observe methods so they show up on the readable structs

--- src/NexusMods.MnemonicDB.Abstractions/Models/IModelFactory.cs
+++ src/NexusMods.MnemonicDB.Abstractions/Models/IModelFactory.cs
@@ -21,32 +21,4 @@ public interface IModelFactory<TFactory, TModel>
     /// The primary attribute of the model, this is used to detect the valid type of the model
     /// </summary>
     public static abstract IAttribute PrimaryAttribute { get; }
-
-    /// <summary>
-    /// Observe a model from the database, the stream ends when the model is deleted or otherwise invalidated
-    /// </summary>
-    public static virtual IObservable<TModel> Observe(IConnection conn, EntityId id)
-    {
-        return conn.ObserveDatoms(id)
-            .QueryWhenChanged(d => TModel.Create(conn.Db, id))
-            .TakeWhile(model => model.IsValid());
-    }
-
-    /// <summary>
-    /// Observe all models of this type from the database
-    /// </summary>
-    public static virtual IObservable<IChangeSet<TModel>> ObserveAll(IConnection conn)
-    {
-        return conn.ObserveDatoms(TFactory.PrimaryAttribute)
-            .Transform(d => TFactory.Load(conn.Db, d.E));
-    }
-}
-
-/// <summary>
-/// Extensions for IModelFactory
-/// </summary>
-public static class ModelFactoryExtensions
-{
-
-
 }
