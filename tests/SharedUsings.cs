045b79a0dd93f15e7e0a1749195db98bb867c5d1
halgari <halgari@users.noreply.github.com>
2024-06-28 09:24:04 +0000

    Update submodules

--- tests/SharedUsings.cs
+++ tests/SharedUsings.cs
@@ -1,17 +1,5 @@
 // ReSharper disable RedundantUsingDirective.Global
-
 global using Xunit;
 global using FluentAssertions;
-using System.Runtime.CompilerServices;
-using NexusMods.MnemonicDB.TestModel.Helpers;
-
-public static class Initializer
-{
-    [ModuleInitializer]
-    public static void Init()
-    {
-        VerifierSettings
-            .AddExtraSettings(s =>
-                s.Converters.Add(new ObjectTupleWriter()));
-    }
-}
+global using AutoFixture;
+global using AutoFixture.Xunit2;
