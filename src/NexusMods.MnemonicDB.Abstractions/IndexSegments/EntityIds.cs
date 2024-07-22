347d9f0d310ac93d566400e73a87e007debe137f
erri120 <erri120@fossmailer.de>
2024-07-22 18:46:29 +0200

    Use custom enumerator implementation
    Results in less allocations.

--- src/NexusMods.MnemonicDB.Abstractions/IndexSegments/EntityIds.cs
+++ src/NexusMods.MnemonicDB.Abstractions/IndexSegments/EntityIds.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using NexusMods.MnemonicDB.Abstractions.Models;
 
 namespace NexusMods.MnemonicDB.Abstractions.IndexSegments;
@@ -8,7 +9,8 @@ namespace NexusMods.MnemonicDB.Abstractions.IndexSegments;
 /// <summary>
 /// A subview of an IndexSegment that returns the entity ids of the segment
 /// </summary>
-public struct EntityIds(IndexSegment segment, int start, int end) :
+[PublicAPI]
+public readonly struct EntityIds(IndexSegment segment, int start, int end) :
     IReadOnlyCollection<EntityId>, IIndexSegment<EntityId>
 {
     /// <summary>
@@ -34,19 +36,12 @@ public struct EntityIds(IndexSegment segment, int start, int end) :
         return arr;
     }
 
-    /// <inheritdoc />
-    public IEnumerator<EntityId> GetEnumerator()
-    {
-        for (var i = start; i < end; i++)
-        {
-            yield return this[i];
-        }
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
+    /// <summary>
+    /// Returns an enumerator.
+    /// </summary>
+    public Enumerator GetEnumerator() => new(segment, start, end);
+    IEnumerator<EntityId> IEnumerable<EntityId>.GetEnumerator() => GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
     /// <summary>
     /// Creates a view of these Ids that auto-casts every Id into a model of the given model type
@@ -55,5 +50,44 @@ public struct EntityIds(IndexSegment segment, int start, int end) :
     where TModel : IReadOnlyModel<TModel>
     {
         return new Entities<TModel>(this, db);
+    }
+
+    /// <summary>
+    /// Enumerator.
+    /// </summary>
+    public struct Enumerator : IEnumerator<EntityId>
+    {
+        private readonly IndexSegment _segment;
+        private readonly int _start;
+        private readonly int _end;
+        private int _index;
+
+        internal Enumerator(IndexSegment segment, int start, int end)
+        {
+            _segment = segment;
+            _start = start;
+            _end = end;
+
+            _index = start;
+        }
+
+        /// <inheritdoc/>
+        public EntityId Current { get; private set; } = default!;
+        object IEnumerator.Current => Current;
+
+        /// <inheritdoc/>
+        public bool MoveNext()
+        {
+            if (_index >= _end) return false;
+            Current = _segment[_index + _start].E;
+            _index += 1;
+            return true;
+        }
+
+        /// <inheritdoc/>
+        public void Reset() => _index = 0;
+
+        /// <inheritdoc/>
+        public void Dispose() { }
     }
 }
