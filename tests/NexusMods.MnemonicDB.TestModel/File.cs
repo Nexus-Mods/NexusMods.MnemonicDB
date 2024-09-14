a45f91e8bd9f5809b1263f0cd774e15887fe27ca
halgari <tbaldridge@gmail.com>
2024-09-14 15:30:32 -0600

    Make File.Path indexed again so we don't break tests

--- tests/NexusMods.MnemonicDB.TestModel/File.cs
+++ tests/NexusMods.MnemonicDB.TestModel/File.cs
@@ -7,7 +7,7 @@ namespace NexusMods.MnemonicDB.TestModel;
 public partial class File : IModelDefinition
 {
     private const string Namespace = "NexusMods.MnemonicDB.TestModel.File";
-    public static readonly RelativePathAttribute Path = new(Namespace, nameof(Path)) {IsIndexed = false};
+    public static readonly RelativePathAttribute Path = new(Namespace, nameof(Path)) {IsIndexed = true};
     public static readonly HashAttribute Hash = new(Namespace, nameof(Hash)) {IsIndexed = true};
     public static readonly SizeAttribute Size = new(Namespace, nameof(Size));
     public static readonly ReferenceAttribute<Mod> Mod = new(Namespace, nameof(Mod));
