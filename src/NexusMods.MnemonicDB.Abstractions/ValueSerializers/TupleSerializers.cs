778df4f5d8cd5aa350f31f696ebb161078f30186
halgari <tbaldridge@gmail.com>
2024-10-17 16:39:46 -0600

    Fix tests

--- src/NexusMods.MnemonicDB.Abstractions/ValueSerializers/TupleSerializers.cs
+++ src/NexusMods.MnemonicDB.Abstractions/ValueSerializers/TupleSerializers.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Buffers;
+using System.Runtime.InteropServices;
 using System.Text;
 using NexusMods.MnemonicDB.Abstractions.ElementComparers;
+using Reloaded.Memory.Extensions;
 
 namespace NexusMods.MnemonicDB.Abstractions.ValueSerializers;
 
@@ -30,19 +32,27 @@ public sealed class Tuple2_UShort_Utf8I_Serializer : IValueSerializer<(ushort, s
         return string.Compare(aStr, bStr, StringComparison.Ordinal);    
     }
 
+    /// <inheritdoc />
     public static (ushort, string) Read(ReadOnlySpan<byte> span)
     {
-        throw new NotImplementedException();
+        var item1 = MemoryMarshal.Read<ushort>(span);
+        var item2 = Encoding.UTF8.GetString(span.SliceFast(sizeof(ushort)).ToArray());
+        return (item1, item2);
     }
 
+    /// <inheritdoc />
     public static void Write<TWriter>((ushort, string) value, TWriter writer) where TWriter : IBufferWriter<byte>
     {
-        throw new NotImplementedException();
+        var span = writer.GetSpan(sizeof(ushort) + Encoding.UTF8.GetByteCount(value.Item2));
+        MemoryMarshal.Write(span, value.Item1);
+        Encoding.UTF8.GetBytes(value.Item2, span.SliceFast(sizeof(ushort)));
+        writer.Advance(sizeof(ushort) + Encoding.UTF8.GetByteCount(value.Item2));
     }
 
+    /// <inheritdoc />
     public static void Remap(Span<byte> span, Func<EntityId, EntityId> remapFn)
     {
-        throw new NotImplementedException();
+        // No-op
     }
 }
 
@@ -78,18 +88,28 @@ public sealed class Tuple3_Ref_UShort_Utf8I_Serializer : IValueSerializer<(Entit
         return string.Compare(aStr, bStr, StringComparison.Ordinal);
     }
 
-    public static (EntityId, ushort, string) Read(ReadOnlySpan<byte> span)
+    /// <inheritdoc />
+    public static unsafe (EntityId, ushort, string) Read(ReadOnlySpan<byte> span)
     {
-        throw new NotImplementedException();
+        var item1 = MemoryMarshal.Read<EntityId>(span);
+        var item2 = MemoryMarshal.Read<ushort>(span.SliceFast(sizeof(EntityId)));
+        var item3 = Encoding.UTF8.GetString(span.SliceFast(sizeof(EntityId) + sizeof(ushort)).ToArray());
+        return (item1, item2, item3);
     }
 
-    public static void Write<TWriter>((EntityId, ushort, string) value, TWriter writer) where TWriter : IBufferWriter<byte>
+    /// <inheritdoc />
+    public static unsafe void Write<TWriter>((EntityId, ushort, string) value, TWriter writer) where TWriter : IBufferWriter<byte>
     {
-        throw new NotImplementedException();
+        var span = writer.GetSpan(sizeof(EntityId) + sizeof(ushort) + Encoding.UTF8.GetByteCount(value.Item3));
+        MemoryMarshal.Write(span, value.Item1);
+        MemoryMarshal.Write(span.SliceFast(sizeof(EntityId)), value.Item2);
+        Encoding.UTF8.GetBytes(value.Item3, span.SliceFast(sizeof(EntityId) + sizeof(ushort)));
+        writer.Advance(sizeof(EntityId) + sizeof(ushort) + Encoding.UTF8.GetByteCount(value.Item3));
     }
 
+    /// <inheritdoc />
     public static void Remap(Span<byte> span, Func<EntityId, EntityId> remapFn)
     {
-        throw new NotImplementedException();
+        MemoryMarshal.Write(span, remapFn(MemoryMarshal.Read<EntityId>(span)));
     }
 }
