2fa05bc0e939796edabb4d2492063f1f519954d9
Timothy Baldridge <tbaldridge@gmail.com>
2025-06-04 16:58:29 -0600

    Fixes found in main (#148)
    * Fix latest tx for entity to account for deleted entities
    
    * Add user defined defaults for DbOrDefault

--- src/NexusMods.MnemonicDB.Abstractions/Cascade/Flows/LatestTxForEntity.cs
+++ src/NexusMods.MnemonicDB.Abstractions/Cascade/Flows/LatestTxForEntity.cs
@@ -22,7 +22,7 @@ public class LatestTxForEntity : Flow<(EntityId Entity, EntityId Tx)>
     private class LatestTxForEntityNode(Topology topology, LatestTxForEntity flow) : Node<(EntityId Entity, EntityId Tx)>(topology, flow, 1)
     {
         private IDb? _db;
-        private Dictionary<EntityId, EntityId> _latestTx = new();
+        private Dictionary<EntityId, (EntityId LatestTx, int DatomCount)> _latestTx = new();
         private Dictionary<EntityId, EntityId> _toEmit = new();
         
         public override void Accept<TIn>(int idx, IToDiffSpan<TIn> diffs)
@@ -39,8 +39,11 @@ public class LatestTxForEntity : Flow<(EntityId Entity, EntityId Tx)>
                 {
                     using var datoms = update.Next!.LightweightDatoms(SliceDescriptor.Create(IndexType.EAVTCurrent), true);
                     var currentEId = EntityId.From(0);
-                    // We have to assign it to something, and intially it will be 0
-                    ref var currentSlot = ref currentEId;
+                    
+                    // Allocate a reference on the stack to the tuple, and initially set it to the current entity and count.
+                    var storageInit = (Tx: EntityId.From(0), Count:0);
+                    ref var currentSlot = ref storageInit;
+                    
                     while (datoms.MoveNext())
                     {
                         var prefix = datoms.KeyPrefix;
@@ -50,14 +53,18 @@ public class LatestTxForEntity : Flow<(EntityId Entity, EntityId Tx)>
 
                         if (prefix.E != currentEId)
                         {
+                            // On the next entity, so update our temporary storage.
                             currentSlot = ref CollectionsMarshal.GetValueRefOrAddDefault(_latestTx, prefix.E, out _);
+                            currentEId = prefix.E;
                         }
-                        currentSlot = txEntity > currentSlot ? txEntity : currentSlot;
+
+                        currentSlot.Tx = txEntity;
+                        currentSlot.Count++;
                     }
 
-                    foreach (var (entityId, txId) in _latestTx)
+                    foreach (var (entityId, state) in _latestTx)
                     {
-                        Output.Add((entityId, txId), 1);
+                        Output.Add((entityId, state.LatestTx), 1);
                     }
 
                     break;
@@ -66,23 +73,42 @@ public class LatestTxForEntity : Flow<(EntityId Entity, EntityId Tx)>
                 {
                     _toEmit.Clear();
                     
+                    var newTx = EntityId.From(update.Next!.BasisTxId.Value);
+                    
                     foreach (var datom in update.Next!.RecentlyAdded)
                     {
                         if (datom.E.InPartition(PartitionId.Transactions))
                             continue;
                         ref var slot = ref CollectionsMarshal.GetValueRefOrAddDefault(_latestTx, datom.E, out var added);
                         var txEntity = EntityId.From(datom.T.Value);
-                        if (txEntity > slot)
+                        if (txEntity > slot.LatestTx)
                         {
-                            _toEmit.TryAdd(datom.E, slot);
-                            slot = txEntity;
+                            // Do try/add because we want to set the value to the old transaction entity, and we don't want to overwrite it if it already exists.
+                            _toEmit.TryAdd(datom.E, slot.LatestTx);
+                            slot.LatestTx = newTx;
                         }
+                        slot.DatomCount += (datom.Prefix.IsRetract ? -1 : 1);
                     }
 
                     foreach (var (e, oldTx) in _toEmit)
                     {
-                        Output.Add((e, oldTx), -1);
-                        Output.Add((e, _latestTx[e]), 1);
+                        var count = _latestTx[e].DatomCount;
+                        
+                        // No datoms left for this entity, so we completely remove the entry
+                        if (count == 0)
+                        {
+                            _latestTx.Remove(e);
+                            Output.Add((e, oldTx), -1);
+                        }
+                        else if (oldTx.Value == 0)
+                        {
+                            Output.Add((e, newTx), 1);
+                        }
+                        else
+                        {
+                            Output.Add((e, oldTx), -1);
+                            Output.Add((e, newTx), 1);
+                        }
                     }
                     
                     _toEmit.Clear();
@@ -90,9 +116,9 @@ public class LatestTxForEntity : Flow<(EntityId Entity, EntityId Tx)>
                 }
                 case UpdateType.RemoveAndAdd:
                 {
-                    foreach (var (e, tx) in _latestTx)
+                    foreach (var (e, slot) in _latestTx)
                     {
-                        Output.Add((e, tx), -1);
+                        Output.Add((e, slot.LatestTx), -1);
                     }
                     _latestTx.Clear();
                     
@@ -115,7 +141,7 @@ public class LatestTxForEntity : Flow<(EntityId Entity, EntityId Tx)>
         {
             foreach (var (entityId, txId) in _latestTx)
             {
-                Output.Add((entityId, txId), 1);
+                Output.Add((entityId, txId.LatestTx), 1);
             }
         }
     }
