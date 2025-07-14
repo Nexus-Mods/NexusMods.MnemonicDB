6ef2555afaac7cdb870a3d5522fc104c15b71f9a
Timothy Baldridge <tbaldridge@gmail.com>
2025-07-14 09:20:04 -0600

    Fix how we call into native deps

--- src/NexusMods.HyperDuck/InternalConsts.cs
+++ src/NexusMods.HyperDuck/InternalConsts.cs
@@ -1,8 +1,20 @@
-﻿namespace NexusMods.HyperDuck;
+﻿using System;
+
+namespace NexusMods.HyperDuck;
 
 internal static class InternalConsts
 {
-    public const string LibraryName = "duckdb";
+
+    #if IS_WINDOWS
+    public const string LibraryName = "runtimes/win-x64/native/duckdb.dll";
+    #elif IS_LINUX
+    public const string LibraryName = "runtimes/linux-x64/native/duckdb.so";
+    #elif IS_MACOS
+    public const string LibraryName = "runtimes/osx-arm64/native/duckdb.dylib";
+    #else
+    public const string LibraryName = "NO_KNOWN_DLL_MAPPING FOR THIS OS";
+    #endif
+    
     
     public const int DefaultVectorSize = 1024 * 2;
     
