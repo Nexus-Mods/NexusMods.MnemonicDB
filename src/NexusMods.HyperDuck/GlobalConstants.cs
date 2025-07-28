6d0dc8a7ca9771d5353af6d9bfae98ac97e9e3c2
Timothy Baldridge <tbaldridge@gmail.com>
2025-07-28 15:39:16 -0600

    See if removing the extension helps

--- src/NexusMods.HyperDuck/GlobalConstants.cs
+++ src/NexusMods.HyperDuck/GlobalConstants.cs
@@ -7,11 +7,11 @@ public static class GlobalConstants
 {
 
     #if IS_WINDOWS
-    public const string LibraryName = "runtimes/win-x64/native/duckdb.dll";
+    public const string LibraryName = "runtimes/win-x64/native/duckdb";
     #elif IS_LINUX
-    public const string LibraryName = "runtimes/linux-x64/native/duckdb.so";
+    public const string LibraryName = "runtimes/linux-x64/native/duckdb";
     #elif IS_MACOS
-    public const string LibraryName = "runtimes/osx-arm64/native/duckdb.dylib";
+    public const string LibraryName = "runtimes/osx-arm64/native/duckdb";
     #else
     public const string LibraryName = "NO_KNOWN_DLL_MAPPING FOR THIS OS";
     #endif
