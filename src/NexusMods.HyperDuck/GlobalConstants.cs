64a8cc4379f187116c55ace234e365288d89e252
Timothy Baldridge <tbaldridge@gmail.com>
2025-07-28 16:08:08 -0600

    Further attempt to fix the broken tests

--- src/NexusMods.HyperDuck/GlobalConstants.cs
+++ src/NexusMods.HyperDuck/GlobalConstants.cs
@@ -7,11 +7,11 @@ public static class GlobalConstants
 {
 
     #if IS_WINDOWS
-    public const string LibraryName = "runtimes/win-x64/native/duckdb";
+    public const string LibraryName = "runtimes/win-x64/native/duckdb.dll";
     #elif IS_LINUX
-    public const string LibraryName = "runtimes/linux-x64/native/duckdb";
+    public const string LibraryName = "runtimes/linux-x64/native/duckdb.so";
     #elif IS_MACOS
-    public const string LibraryName = "runtimes/osx-arm64/native/duckdb";
+    public const string LibraryName = "runtimes/osx-arm64/native/duckdb.dylib";
     #else
     public const string LibraryName = "NO_KNOWN_DLL_MAPPING FOR THIS OS";
     #endif
