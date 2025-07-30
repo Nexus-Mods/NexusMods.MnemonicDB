fa22cce5ec5c20d87422e9d14fa583e26e57ea28
Timothy Baldridge <tbaldridge@gmail.com>
2025-07-29 22:23:52 -0600

    Comment out cleanup logic in `LiveQueryUpdater.Dispose` for further investigation.

--- src/NexusMods.HyperDuck/Internals/LiveQueryUpdater.cs
+++ src/NexusMods.HyperDuck/Internals/LiveQueryUpdater.cs
@@ -57,11 +57,14 @@ public class LiveQueryUpdater : IDisposable
 
     public void Dispose()
     {
+        // What happens if we never clean up?
+        /*
         _cancelationToken.Cancel();
         _task?.Wait();
         _task = null;
         _liveQueries.Clear();
         _pendingFlushes = ImmutableStack<TaskCompletionSource>.Empty;
+        */
     }
 
     public void Remove(ILiveQuery liveQuery)
