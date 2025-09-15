9c4f6da7e66db94f32a8947d0a03bfe45592b8d8
erri120 <erri120@fossmailer.de>
2025-09-15 13:13:09 +0200

    Implement `IValueAdaptorFactory` for `TupleAdaptorFactory`

--- src/NexusMods.HyperDuck/Adaptor/Impls/RowAdaptors/TupleAdaptor.cs
+++ src/NexusMods.HyperDuck/Adaptor/Impls/RowAdaptors/TupleAdaptor.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace NexusMods.HyperDuck.Adaptor.Impls.RowAdaptors;
 
-public class TupleAdaptor<T1, T2, TAdaptor1, TAdaptor2> : IRowAdaptor<(T1, T2)>
+public class TupleAdaptor<T1, T2, TAdaptor1, TAdaptor2> : IRowAdaptor<(T1, T2)>, IValueAdaptor<(T1, T2)>
     where TAdaptor1 : IValueAdaptor<T1>
     where TAdaptor2 : IValueAdaptor<T2>
 {
@@ -14,9 +14,14 @@ public class TupleAdaptor<T1, T2, TAdaptor1, TAdaptor2> : IRowAdaptor<(T1, T2)>
         valCursor.ColumnIndex++;
         TAdaptor2.Adapt(valCursor, ref value.Item2!);
     }
+
+    public static void Adapt<TCursor>(TCursor cursor, ref (T1, T2) value) where TCursor : IValueCursor, allows ref struct
+    {
+        throw new NotImplementedException();
+    }
 }
 
-public class TupleAdaptor<T1, T2, T3, TAdaptor1, TAdaptor2, TAdaptor3> : IRowAdaptor<(T1, T2, T3)>
+public class TupleAdaptor<T1, T2, T3, TAdaptor1, TAdaptor2, TAdaptor3> : IRowAdaptor<(T1, T2, T3)>, IValueAdaptor<(T1, T2, T3)>
     where TAdaptor1 : IValueAdaptor<T1>
     where TAdaptor2 : IValueAdaptor<T2>
     where TAdaptor3 : IValueAdaptor<T3>
@@ -30,10 +35,14 @@ public class TupleAdaptor<T1, T2, T3, TAdaptor1, TAdaptor2, TAdaptor3> : IRowAda
         valCursor.ColumnIndex++;
         TAdaptor3.Adapt(valCursor, ref value.Item3!);
     }
+
+    public static void Adapt<TCursor>(TCursor cursor, ref (T1, T2, T3) value) where TCursor : IValueCursor, allows ref struct
+    {
+        throw new NotImplementedException();
+    }
 }
 
-
-public class TupleAdaptor<T1, T2, T3, T4, TAdaptor1, TAdaptor2, TAdaptor3, TAdaptor4> : IRowAdaptor<(T1, T2, T3, T4)>
+public class TupleAdaptor<T1, T2, T3, T4, TAdaptor1, TAdaptor2, TAdaptor3, TAdaptor4> : IRowAdaptor<(T1, T2, T3, T4)>, IValueAdaptor<(T1, T2, T3, T4)>
     where TAdaptor1 : IValueAdaptor<T1>
     where TAdaptor2 : IValueAdaptor<T2>
     where TAdaptor3 : IValueAdaptor<T3>
@@ -50,10 +59,15 @@ public class TupleAdaptor<T1, T2, T3, T4, TAdaptor1, TAdaptor2, TAdaptor3, TAdap
         valCursor.ColumnIndex++;
         TAdaptor4.Adapt(valCursor, ref value.Item4!);
     }
+
+    public static void Adapt<TCursor>(TCursor cursor, ref (T1, T2, T3, T4) value) where TCursor : IValueCursor, allows ref struct
+    {
+        throw new NotImplementedException();
+    }
 }
 
 
-public class TupleAdaptor<T1, T2, T3, T4, T5, TAdaptor1, TAdaptor2, TAdaptor3, TAdaptor4, TAdaptor5> : IRowAdaptor<(T1, T2, T3, T4, T5)>
+public class TupleAdaptor<T1, T2, T3, T4, T5, TAdaptor1, TAdaptor2, TAdaptor3, TAdaptor4, TAdaptor5> : IRowAdaptor<(T1, T2, T3, T4, T5)>, IValueAdaptor<(T1, T2, T3, T4, T5)>
     where TAdaptor1 : IValueAdaptor<T1>
     where TAdaptor2 : IValueAdaptor<T2>
     where TAdaptor3 : IValueAdaptor<T3>
@@ -73,9 +87,14 @@ public class TupleAdaptor<T1, T2, T3, T4, T5, TAdaptor1, TAdaptor2, TAdaptor3, T
         valCursor.ColumnIndex++;
         TAdaptor5.Adapt(valCursor, ref value.Item5!);
     }
+
+    public static void Adapt<TCursor>(TCursor cursor, ref (T1, T2, T3, T4, T5) value) where TCursor : IValueCursor, allows ref struct
+    {
+        throw new NotImplementedException();
+    }
 }
 
-public class TupleAdaptor<T1, T2, T3, T4, T5, T6, TAdaptor1, TAdaptor2, TAdaptor3, TAdaptor4, TAdaptor5, TAdaptor6> : IRowAdaptor<(T1, T2, T3, T4, T5, T6)>
+public class TupleAdaptor<T1, T2, T3, T4, T5, T6, TAdaptor1, TAdaptor2, TAdaptor3, TAdaptor4, TAdaptor5, TAdaptor6> : IRowAdaptor<(T1, T2, T3, T4, T5, T6)>, IValueAdaptor<(T1, T2, T3, T4, T5, T6)>
     where TAdaptor1 : IValueAdaptor<T1>
     where TAdaptor2 : IValueAdaptor<T2>
     where TAdaptor3 : IValueAdaptor<T3>
@@ -98,9 +117,14 @@ public class TupleAdaptor<T1, T2, T3, T4, T5, T6, TAdaptor1, TAdaptor2, TAdaptor
         valCursor.ColumnIndex++;
         TAdaptor6.Adapt(valCursor, ref value.Item6!);
     }
+
+    public static void Adapt<TCursor>(TCursor cursor, ref (T1, T2, T3, T4, T5, T6) value) where TCursor : IValueCursor, allows ref struct
+    {
+        throw new NotImplementedException();
+    }
 }
 
-public class TupleAdaptor<T1, T2, T3, T4, T5, T6, T7, TAdaptor1, TAdaptor2, TAdaptor3, TAdaptor4, TAdaptor5, TAdaptor6, TAdaptor7> : IRowAdaptor<(T1, T2, T3, T4, T5, T6, T7)>
+public class TupleAdaptor<T1, T2, T3, T4, T5, T6, T7, TAdaptor1, TAdaptor2, TAdaptor3, TAdaptor4, TAdaptor5, TAdaptor6, TAdaptor7> : IRowAdaptor<(T1, T2, T3, T4, T5, T6, T7)>, IValueAdaptor<(T1, T2, T3, T4, T5, T6, T7)>
     where TAdaptor1 : IValueAdaptor<T1>
     where TAdaptor2 : IValueAdaptor<T2>
     where TAdaptor3 : IValueAdaptor<T3>
@@ -126,9 +150,14 @@ public class TupleAdaptor<T1, T2, T3, T4, T5, T6, T7, TAdaptor1, TAdaptor2, TAda
         valCursor.ColumnIndex++;
         TAdaptor7.Adapt(valCursor, ref value.Item7!);
     }
+
+    public static void Adapt<TCursor>(TCursor cursor, ref (T1, T2, T3, T4, T5, T6, T7) value) where TCursor : IValueCursor, allows ref struct
+    {
+        throw new NotImplementedException();
+    }
 }
 
-public class TupleAdaptor<T1, T2, T3, T4, T5, T6, T7, T8, TAdaptor1, TAdaptor2, TAdaptor3, TAdaptor4, TAdaptor5, TAdaptor6, TAdaptor7, TAdaptor8> : IRowAdaptor<(T1, T2, T3, T4, T5, T6, T7, T8)>
+public class TupleAdaptor<T1, T2, T3, T4, T5, T6, T7, T8, TAdaptor1, TAdaptor2, TAdaptor3, TAdaptor4, TAdaptor5, TAdaptor6, TAdaptor7, TAdaptor8> : IRowAdaptor<(T1, T2, T3, T4, T5, T6, T7, T8)>, IValueAdaptor<(T1, T2, T3, T4, T5, T6, T7, T8)>
     where TAdaptor1 : IValueAdaptor<T1>
     where TAdaptor2 : IValueAdaptor<T2>
     where TAdaptor3 : IValueAdaptor<T3>
@@ -157,50 +186,46 @@ public class TupleAdaptor<T1, T2, T3, T4, T5, T6, T7, T8, TAdaptor1, TAdaptor2, 
         valCursor.ColumnIndex++;
         TAdaptor8.Adapt(valCursor, ref value.Item8!);
     }
+
+    public static void Adapt<TCursor>(TCursor cursor, ref (T1, T2, T3, T4, T5, T6, T7, T8) value) where TCursor : IValueCursor, allows ref struct
+    {
+        throw new NotImplementedException();
+    }
 }
 
-public class TupleAdaptorFactory : IRowAdaptorFactory
+public class TupleAdaptorFactory : IRowAdaptorFactory, IValueAdaptorFactory
 {
-    public bool TryExtractElementTypes(ReadOnlySpan<Result.ColumnInfo> result, Type resultType, out Type[] elementTypes, out int priority)
+    private static bool TryExtractTypes(Type resultType, out Type[] elementTypes, out int priority)
     {
         priority = 0;
         elementTypes = [];
-        if (!resultType.IsAssignableTo(typeof(ITuple)))
-            return false;
-        
-        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,>), out elementTypes)) 
-            return true;
-        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,>), out elementTypes))
-            return true;
-        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,,>), out elementTypes))
-            return true;
-        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,,,>), out elementTypes))
-            return true;
-        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,,,,>), out elementTypes))
-            return true;
-        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,,,,,>), out elementTypes))
-            return true;
-        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,,,,,,>), out elementTypes))
-            return true;
+
+        if (!resultType.IsAssignableTo(typeof(ITuple))) return false;
+        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,>), out elementTypes)) return true;
+        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,>), out elementTypes)) return true;
+        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,,>), out elementTypes)) return true;
+        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,,,>), out elementTypes)) return true;
+        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,,,,>), out elementTypes)) return true;
+        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,,,,,>), out elementTypes)) return true;
+        if (resultType.TryExtractGenericInterfaceArguments(typeof(ValueTuple<,,,,,,,>), out elementTypes)) return true;
         return false;
     }
 
-    public Type CreateType(Type resultType, Type[] elementTypes, Type[] elementAdaptors)
+    private static Type CreateType(Type[] elementTypes, Type[] elementAdaptors)
     {
-        if (elementTypes.Length == 2)
-            return typeof(TupleAdaptor<,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementAdaptors[0], elementAdaptors[1]);
-        if (elementTypes.Length == 3)
-            return typeof(TupleAdaptor<,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2]);
-        if (elementTypes.Length == 4)
-            return typeof(TupleAdaptor<,,,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementTypes[3], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2], elementAdaptors[3]);
-        if (elementTypes.Length == 5)
-            return typeof(TupleAdaptor<,,,,,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementTypes[3], elementTypes[4], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2], elementAdaptors[3], elementAdaptors[4]);
-        if (elementTypes.Length == 6)
-            return typeof(TupleAdaptor<,,,,,,,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementTypes[3], elementTypes[4], elementTypes[5], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2], elementAdaptors[3], elementAdaptors[4], elementAdaptors[5]);
-        if (elementTypes.Length == 7)
-            return typeof(TupleAdaptor<,,,,,,,,,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementTypes[3], elementTypes[4], elementTypes[5], elementTypes[6], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2], elementAdaptors[3], elementAdaptors[4], elementAdaptors[5], elementAdaptors[6]);
-        if (elementTypes.Length == 8)
-            return typeof(TupleAdaptor<,,,,,,,,,,,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementTypes[3], elementTypes[4], elementTypes[5], elementTypes[6], elementTypes[7], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2], elementAdaptors[3], elementAdaptors[4], elementAdaptors[5], elementAdaptors[6], elementAdaptors[7]);
+        if (elementTypes.Length == 2) return typeof(TupleAdaptor<,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementAdaptors[0], elementAdaptors[1]);
+        if (elementTypes.Length == 3) return typeof(TupleAdaptor<,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2]);
+        if (elementTypes.Length == 4) return typeof(TupleAdaptor<,,,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementTypes[3], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2], elementAdaptors[3]);
+        if (elementTypes.Length == 5) return typeof(TupleAdaptor<,,,,,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementTypes[3], elementTypes[4], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2], elementAdaptors[3], elementAdaptors[4]);
+        if (elementTypes.Length == 6) return typeof(TupleAdaptor<,,,,,,,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementTypes[3], elementTypes[4], elementTypes[5], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2], elementAdaptors[3], elementAdaptors[4], elementAdaptors[5]);
+        if (elementTypes.Length == 7) return typeof(TupleAdaptor<,,,,,,,,,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementTypes[3], elementTypes[4], elementTypes[5], elementTypes[6], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2], elementAdaptors[3], elementAdaptors[4], elementAdaptors[5], elementAdaptors[6]);
+        if (elementTypes.Length == 8) return typeof(TupleAdaptor<,,,,,,,,,,,,,,,>).MakeGenericType(elementTypes[0], elementTypes[1], elementTypes[2], elementTypes[3], elementTypes[4], elementTypes[5], elementTypes[6], elementTypes[7], elementAdaptors[0], elementAdaptors[1], elementAdaptors[2], elementAdaptors[3], elementAdaptors[4], elementAdaptors[5], elementAdaptors[6], elementAdaptors[7]);
         throw new NotImplementedException();
     }
+
+    bool IRowAdaptorFactory.TryExtractElementTypes(ReadOnlySpan<Result.ColumnInfo> result, Type resultType, out Type[] elementTypes, out int priority) => TryExtractTypes(resultType, out elementTypes, out priority);
+    Type IRowAdaptorFactory.CreateType(Type resultType, Type[] elementTypes, Type[] elementAdaptors) => CreateType(elementTypes, elementAdaptors);
+
+    bool IValueAdaptorFactory.TryExtractType(DuckDbType taggedType, LogicalType logicalType, Type type, out Type[] subTypes, out int priority) => TryExtractTypes(type, out subTypes, out priority);
+    Type IValueAdaptorFactory.CreateType(Registry registry, DuckDbType taggedType, LogicalType logicalType, Type resultTypes, Type[] subTypes, Type[] subAdaptors) => CreateType(subTypes, subAdaptors);
 }
