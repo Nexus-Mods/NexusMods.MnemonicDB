6bba53ab477361f85ff0d316ba30a305a9c33199
erri120 <erri120@fossmailer.de>
2025-09-30 10:10:44 +0200

    Support `TimestampNs` for `DateTimeOffsetAdaptorFactory`

--- src/NexusMods.HyperDuck/Adaptor/Impls/ValueAdaptor/DateTimeOffsetAdaptor.cs
+++ src/NexusMods.HyperDuck/Adaptor/Impls/ValueAdaptor/DateTimeOffsetAdaptor.cs
@@ -2,12 +2,23 @@ using System;
 
 namespace NexusMods.HyperDuck.Adaptor.Impls.ValueAdaptor;
 
-public class DateTimeOffsetAdaptor : IValueAdaptor<DateTimeOffset>
+public class DateTimeOffsetTicksAdaptor : IValueAdaptor<DateTimeOffset>
 {
     public static bool Adapt<TCursor>(TCursor cursor, ref DateTimeOffset value) where TCursor : IValueCursor, allows ref struct
     {
-        var longValue = cursor.GetValue<long>();
-        return Helpers.AssignNotEq(ref value, new DateTimeOffset(longValue, TimeSpan.Zero));
+        var ticks = cursor.GetValue<long>();
+        return Helpers.AssignNotEq(ref value, new DateTimeOffset(ticks, TimeSpan.Zero));
+    }
+}
+
+public class DateTimeOffsetNanoAdaptor : IValueAdaptor<DateTimeOffset>
+{
+    public static bool Adapt<TCursor>(TCursor cursor, ref DateTimeOffset value) where TCursor : IValueCursor, allows ref struct
+    {
+        // NOTE(erri120): TIMESTAMP_NS is stored as nanoseconds since 1970-01-01 in int64_t.
+        var nanoseconds = cursor.GetValue<long>();
+        var ticks = nanoseconds / TimeSpan.NanosecondsPerTick;
+        return Helpers.AssignNotEq(ref value, new DateTimeOffset(ticks, TimeSpan.Zero));
     }
 }
 
@@ -15,21 +26,22 @@ public class DateTimeOffsetAdaptorFactory : IValueAdaptorFactory
 {
     public bool TryExtractType(DuckDbType taggedType, LogicalType logicalType, Type type, out Type[] subTypes, out int priority)
     {
-        if (taggedType == DuckDbType.BigInt && type == typeof(DateTimeOffset))
+        if ((taggedType is DuckDbType.BigInt or DuckDbType.TimestampNs) && type == typeof(DateTimeOffset))
         {
             priority = 1;
             subTypes = [];
             return true;
         }
-        
+
         priority = 0;
         subTypes = [];
         return false;
     }
 
-    public Type CreateType(Registry registry, DuckDbType taggedType, LogicalType logicalType, Type resultTypes,
-        Type[] subTypes, Type[] subAdaptors)
+    public Type CreateType(Registry registry, DuckDbType taggedType, LogicalType logicalType, Type resultTypes, Type[] subTypes, Type[] subAdaptors)
     {
-        return typeof(DateTimeOffsetAdaptor);
+        if (taggedType is DuckDbType.TimestampNs) return typeof(DateTimeOffsetNanoAdaptor);
+        if (taggedType is DuckDbType.BigInt) return typeof(DateTimeOffsetTicksAdaptor);
+        throw new NotSupportedException();
     }
 }
