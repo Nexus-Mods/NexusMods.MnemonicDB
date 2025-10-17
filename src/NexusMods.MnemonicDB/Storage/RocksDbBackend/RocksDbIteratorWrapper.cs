6a57bed020018ccbc77c5356ac25be04019935f2
Timothy Baldridge <tbaldridge@gmail.com>
2025-10-17 11:46:42 -0600

    Refactor `RocksDbIteratorWrapper` to use primary constructor and simplify logic

--- src/NexusMods.MnemonicDB/Storage/RocksDbBackend/RocksDbIteratorWrapper.cs
+++ src/NexusMods.MnemonicDB/Storage/RocksDbBackend/RocksDbIteratorWrapper.cs
@@ -5,41 +5,33 @@ using RocksDbSharp;
 
 namespace NexusMods.MnemonicDB.Storage.RocksDbBackend;
 
-internal struct RocksDbIteratorWrapper : ILowLevelIterator, IRefDatomPeekingEnumerator
+internal struct RocksDbIteratorWrapper(Iterator iterator) : ILowLevelIterator, IRefDatomPeekingEnumerator
 {
     private Ptr _key;
-    private bool _started;
-    private readonly Iterator _iterator;
-
-    public RocksDbIteratorWrapper(Iterator iterator)
-    {
-        _iterator = iterator;
-        _started = false;
-    }
+    private bool _started = false;
 
     public void Dispose() => 
-        _iterator.Dispose();
+        iterator.Dispose();
 
     public unsafe bool MoveNext<TSliceDescriptor>(TSliceDescriptor descriptor, bool useHistory = false) 
         where TSliceDescriptor : ISliceDescriptor, allows ref struct
     {
-        if (_started == false)
+        if (!_started)
         {
             descriptor.Reset(this, useHistory);
             _started = true;
         }
         else
         {
-            descriptor.MoveNext(this);
+            iterator.Next();
         }
-            
-        if (_iterator.Valid())
-        {
-            var kPtr = Native.Instance.rocksdb_iter_key(_iterator.Handle, out var kLen);
-            _key = new Ptr((byte*)kPtr, (int)kLen);
-            return descriptor.ShouldContinue(_key.Span, useHistory);
-        }
-        return false;
+
+        if (!iterator.Valid()) 
+            return false;
+        
+        var kPtr = Native.Instance.rocksdb_iter_key(iterator.Handle, out var kLen);
+        _key = new Ptr((byte*)kPtr, (int)kLen);
+        return descriptor.ShouldContinue(_key.Span, useHistory);
     }
 
 
@@ -51,7 +43,7 @@ internal struct RocksDbIteratorWrapper : ILowLevelIterator, IRefDatomPeekingEnum
     {
         get 
         {
-            var vPtr = Native.Instance.rocksdb_iter_value(_iterator.Handle, out var vLen);
+            var vPtr = Native.Instance.rocksdb_iter_value(iterator.Handle, out var vLen);
             return new Ptr((byte*)vPtr, (int)vLen);
         }
     }
@@ -63,20 +55,20 @@ internal struct RocksDbIteratorWrapper : ILowLevelIterator, IRefDatomPeekingEnum
 
 
     public void SeekTo(ReadOnlySpan<byte> span) => 
-        _iterator.Seek(span);
+        iterator.Seek(span);
 
     public void SeekToPrev(ReadOnlySpan<byte> span)
     {
-        _iterator.Seek(span);
-        if (!_iterator.Valid())
-            _iterator.SeekToLast();
+        iterator.Seek(span);
+        if (!iterator.Valid())
+            iterator.SeekToLast();
         else
-            _iterator.Prev();
+            iterator.Prev();
     }
 
     public void Next() => 
-        _iterator.Next();
+        iterator.Next();
 
     public void Prev() => 
-        _iterator.Prev();
+        iterator.Prev();
 }
